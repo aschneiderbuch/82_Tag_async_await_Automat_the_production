0de857da71a74995118a3c0715d1954602337770
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-04-08 20:49:42 +0200

    fE fetch money async

--- frontEnd/react-app/src/pages/HomePage.jsx
+++ frontEnd/react-app/src/pages/HomePage.jsx
@@ -8,24 +8,43 @@ import { OutputAnzeige } from '../components/OutputAnzeige'
 import { Sell } from '../components/Sell'
 
 export const HomePage = () => {
-const [money, setMoney] = useState()
-const [rerender, setRerender] = useState(false)
-    useEffect((effect) => {
+  const [money, setMoney] = useState()
+  const [rerender, setRerender] = useState(false)
+     useEffect( (effect) => {
 
-        fetch('http://localhost:9999/api/v1/money')
-        .then(res => res.json())
-        .then(data => setMoney(data))
-    },[rerender])
+  //   fetch('http://localhost:9999/api/v1/money')
+  //   .then(res => res.json())
+  // .then(data => setMoney(data))
+
+  // ! async
+  const fetchData = async () => {
+    // ! try 
+    try {
+      const res = await fetch('http://localhost:9999/api/v1/money')
+      const data = await res.json()
+      setMoney(data)
+    
+      // ! catch
+    }catch(err) { console.log(err)
+  }finally {console.log('react fetch money -->fertig')
+}
+  }
+  fetchData()
+},[rerender])
+
+
+
+
 
   return (
     <section className='homePage'>
-        <h1>{money}$</h1>
+      <h1>{money}$</h1>
 
-<article>
-<InputHuman setRerender={setRerender}></InputHuman>
-<OutputAnzeige rerender={rerender}></OutputAnzeige>
-<Sell setRerender={setRerender}></Sell>
-</article>
+      <article>
+        <InputHuman setRerender={setRerender}></InputHuman>
+        <OutputAnzeige rerender={rerender}></OutputAnzeige>
+        <Sell setRerender={setRerender}></Sell>
+      </article>
     </section>
   )
 }
